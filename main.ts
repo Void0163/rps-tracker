33b1e2cb18b307d97d8035395ee4d9a039c237e5
Void0163 <116900934+Void0163@users.noreply.github.com>
2023-01-31 11:41:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
-    OLED.init(128, 64)
-    pas = 0
-    pbs = 0
-    t = 0
-    r = 0
-    OLED.writeStringNewLine("wanna play a game?")
-    basic.pause(2000)
-    update()
+    reset()
 })
 function update () {
     OLED.clear()
@@ -18,22 +11,21 @@ function update () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:  " + s)
 }
-let r = 0
-let t = 0
-let pbs = 0
-let pas = 0
+function reset () {
+    OLED.init(128, 64)
+    pas2 = 0
+    pbs2 = 0
+    u = 0
+    s = 0
+    OLED.writeStringNewLine("wanna play a game?")
+    basic.pause(2000)
+    update()
+}
 let s = 0
 let u = 0
 let pbs2 = 0
 let pas2 = 0
-OLED.init(128, 64)
-pas2 = 0
-pbs2 = 0
-u = 0
-s = 0
-OLED.writeStringNewLine("wanna play a game?")
-basic.pause(2000)
-update()
+reset()
 basic.forever(function () {
 	
 })
